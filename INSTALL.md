ed4a9d189de7e26193f0e93fc51c984c936c7581
윤정한 <165637494+jeonghanyun@users.noreply.github.com>
2025-10-10 17:26:29 +0900

    ㅁㄴㅇ

---
+++ INSTALL.md
@@ -0,0 +1,9 @@
+composer install
+
+npm install
+
+npm run dev
+
+php artisan migrate:fresh --seed
+
+php artisan serve --port={포트번호}
--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,6 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
             detectTls: false, // Valet TLS 감지 비활성화
-            detectTls: false,
         }),
         tailwindcss(),
     ],
